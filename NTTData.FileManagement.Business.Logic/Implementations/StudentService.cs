59ce6c0f017f867e0a54e661ece53220995dd5fa
aocanaba <aocanaba@emeal.nttdata.com>
2023-01-17 13:26:48 +0100

    Added student scaffolding implementation, with student classes in all layer closes #3

---
+++ NTTData.FileManagement.Business.Logic/Implementations/StudentService.cs
@@ -0,0 +1,20 @@
+﻿using NTTData.FileManagement.Business.Logic.Contracts;
+using NTTData.FileManagement.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTTData.FileManagement.Business.Logic.Implementations
+{
+    public class StudentService:IStudentService
+    {
+        public bool Add(Student student )
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
